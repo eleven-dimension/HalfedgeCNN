fc227c701c4ba92337cf58792234b30b55c3a453
eleven-dimension <3096152319@qq.com>
2024-05-13 16:32:18 +1000

    Sort neighbors.

--- packages/half_edge/neighbor.py
+++ packages/half_edge/neighbor.py
@@ -6,32 +6,32 @@ def get_neighbors_type_A(half_edge: HalfEdge) -> List[HalfEdge]:
 
 
 def get_neighbors_type_B(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.O(), half_edge.N(), half_edge.P()]
+    return [half_edge.N(), half_edge.P(), half_edge.O()]
 
 
 def get_neighbors_type_C(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.O(), half_edge.N(), half_edge.ON()]
+    return [half_edge.N(), half_edge.O(), half_edge.ON()]
 
 
 def get_neighbors_type_D(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.PO(), half_edge.NO(), half_edge.ON(), half_edge.OP()]
+    return [half_edge.NO(), half_edge.PO(), half_edge.ON(), half_edge.OP()]
 
 
 def get_neighbors_type_E(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.PO(), half_edge.NO(), half_edge.N(), half_edge.P(), half_edge.O()]
+    return [half_edge.N(), half_edge.NO(), half_edge.P(), half_edge.PO(), half_edge.O()]
 
 
 def get_neighbors_type_F(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.P(), half_edge.N(), half_edge.ON(), half_edge.OP(), half_edge.O()]
+    return [half_edge.N(), half_edge.P(), half_edge.O(), half_edge.ON(), half_edge.OP()]
 
 
 def get_neighbors_type_G(half_edge: HalfEdge) -> List[HalfEdge]:
-    return [half_edge.P(), half_edge.N(), half_edge.PO(), half_edge.NO(), half_edge.ON(), half_edge.OP(), half_edge.O()]
+    return [half_edge.N(), half_edge.NO(), half_edge.P(), half_edge.PO(), half_edge.O(), half_edge.ON(), half_edge.OP()]
 
 
 def get_neighbors_type_H(half_edge: HalfEdge) -> List[HalfEdge]:
     return [
-        half_edge.P(), half_edge.N(), 
-        half_edge.PO(), half_edge.NO(), half_edge.ON(), half_edge.OP(), half_edge.O(), 
-        half_edge.OPO(), half_edge.ONO()
+        half_edge.N(), half_edge.NO(), 
+        half_edge.P(), half_edge.PO(), 
+        half_edge.O(), half_edge.ON(), half_edge.ONO(), half_edge.OP(), half_edge.OPO()
     ]
