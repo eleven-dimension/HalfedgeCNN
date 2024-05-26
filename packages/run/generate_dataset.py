d06605bb7c13241d611ed4b8d0cef649a0e46b3c
eleven-dimension <3096152319@qq.com>
2024-05-26 21:34:04 +1000

    Add more datapoints.

--- packages/run/generate_dataset.py
+++ packages/run/generate_dataset.py
@@ -10,18 +10,21 @@ def generate_shapes(num_cylinders, num_pyramids):
     # Generate the specified number of cylinders
     for i in range(num_cylinders):
         file_path = f"./packages/obj/dataset/cylinder/cylinder_{i}.obj"
+        # print("c", i)
         generate_random_shape(file_path, 'cylinder')
+        
 
     # Generate the specified number of pyramids
     for i in range(num_pyramids):
         file_path = f"./packages/obj/dataset/pyramid/pyramid_{i}.obj"
+        # print("p", i)
         generate_random_shape(file_path, 'pyramid')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate cylinders and pyramids.")
-    parser.add_argument('--cylinders', type=int, default=1000, help='Number of cylinders to generate')
-    parser.add_argument('--pyramids', type=int, default=1000, help='Number of pyramids to generate')
+    parser.add_argument('--cylinders', type=int, default=2000, help='Number of cylinders to generate')
+    parser.add_argument('--pyramids', type=int, default=2000, help='Number of pyramids to generate')
     args = parser.parse_args()
 
     generate_shapes(args.cylinders, args.pyramids)
